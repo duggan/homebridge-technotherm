36597e83008b6b0238e7fd9151c26c97efd4c628
Ross Duggan <ross@duggan.ie>
2024-02-13 22:31:04 +0000

    Try fixing mode setting

--- src/radiator.ts
+++ src/radiator.ts
@@ -64,8 +64,18 @@ export class Radiator {
       default:
         mode = 'off';
     }
+
+    const targetTemperature = this.service.getCharacteristic(this.platform.Characteristic.TargetTemperature).value;
+    // Check if targetTemperature is null, if so, provide a default value
+    const stemp = targetTemperature !== null ? targetTemperature.toString() : '0';
+
     try {
-      await this.helkiClient.setStatus(this.accessory.context.device.dev_id, this.node, { mode: mode });
+      await this.helkiClient.setStatus(this.accessory.context.device.dev_id, this.node,
+        {
+          mode: mode,
+          stemp: stemp,
+          units: 'C'
+        });
     } catch (error) {
       this.platform.log.error('Failed to set target heating/cooling state:', error);
     }
