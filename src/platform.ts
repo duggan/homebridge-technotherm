94b82dc7afab66b2fbb3e18f0de6cb290591c031
Ross Duggan <ross@duggan.ie>
2024-08-16 13:47:35 +0100

    If there is a network error during plugin init, retry with exponential backoff.

--- src/platform.ts
+++ src/platform.ts
@@ -49,86 +49,95 @@ export class Technotherm implements DynamicPlatformPlugin {
    * Authenticate with the API to obtain an access token and fetch the list of devices.
    */
   async discoverDevices() {
-    try {
-      const helki = new HelkiClient(
-        this.config.apiName,
-        this.config.clientId,
-        this.config.clientSecret,
-        this.config.username,
-        this.config.password,
-        this.log);
+    let backoffDelay = 1000; // Initial delay of 1 second
+    const maxBackoffDelay = 60 * 60 * 1000; // Maximum delay of 1 hour (in milliseconds)
 
-      const groups = await helki.getGroupedDevices();
-      // Filter on home if specified
-      const home = groups.find(home => home.name === this.config.home);
+    // eslint-disable-next-line no-constant-condition
+    while (true) { // Continue indefinitely
+      try {
+        const helki = new HelkiClient(
+          this.config.apiName,
+          this.config.clientId,
+          this.config.clientSecret,
+          this.config.username,
+          this.config.password,
+          this.log,
+        );
 
-      for (const group of groups) {
-        // Loop over the devices in the group
-        // Sometimes a device contains a single radiator, sometimes multiple
-        // Each radiator is a node in the Helki API
-        for (const device of group.devs) {
-          const nodes = await helki.getNodes(device.dev_id);
+        const groups = await helki.getGroupedDevices();
+        // Filter on home if specified
+        const home = groups.find(home => home.name === this.config.home);
 
-          if (nodes.length === 0) {
-            this.log.warn(`No nodes found for device: ${device.name}`);
-            continue;
-          }
+        for (const group of groups) {
+          // Loop over the devices in the group
+          for (const device of group.devs) {
+            const nodes = await helki.getNodes(device.dev_id);
 
-          // Devices can have multiple nodes, register an accessory for each
-          for (const node of nodes) {
-            // Use Node attributes for the accessory display name and UUID gen
-            // but fallback to Device attributes in case they're undefined (i.e. Devices with a single node)
-            const accessoryName = node.name || device.name;
-            const accessoryUUID = this.api.hap.uuid.generate(node.uid || device.dev_id);
-            const existingAccessory = this.accessories.find(accessory => accessory.UUID === accessoryUUID);
+            if (nodes.length === 0) {
+              this.log.warn(`No nodes found for device: ${device.name}`);
+              continue;
+            }
 
-            if (existingAccessory) {
-              // If the existing accessory is not in the configured home, remove it
-              if (home && existingAccessory?.context.home !== home.name) {
-                this.log.warn(`Removing accessory that does not match configured home "${home.name}: ${existingAccessory.displayName}"`);
-                try {
-                  this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-                } catch (error: unknown) {
-                  this.log.warn(`existing accessory: ${error}`);
+            for (const node of nodes) {
+              const accessoryName = node.name || device.name;
+              const accessoryUUID = this.api.hap.uuid.generate(node.uid || device.dev_id);
+              const existingAccessory = this.accessories.find(accessory => accessory.UUID === accessoryUUID);
+
+              if (existingAccessory) {
+                if (home && existingAccessory?.context.home !== home.name) {
+                  this.log.warn(`Removing accessory that does not match configured home "${home.name}: ${existingAccessory.displayName}"`);
+                  try {
+                    this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+                  } catch (error: unknown) {
+                    this.log.warn(`existing accessory: ${error}`);
+                  }
+                } else {
+                  if (existingAccessory.displayName !== accessoryName) {
+                    this.log.info(`Renaming accessory from ${existingAccessory.displayName} to ${accessoryName}`);
+                    existingAccessory.displayName = accessoryName;
+                    this.api.updatePlatformAccessories([existingAccessory]);
+                  }
+
+                  this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+                  new Radiator(this, existingAccessory, helki);
                 }
               } else {
-                // If the node has been renamed, update the corresponding accessory
-                if (existingAccessory.displayName !== accessoryName) {
-                  this.log.info(`Renaming accessory from ${existingAccessory.displayName} to ${accessoryName}`);
-                  existingAccessory.displayName = accessoryName;
-                  this.api.updatePlatformAccessories([existingAccessory]);
+                const accessory = new this.api.platformAccessory(accessoryName, accessoryUUID);
+                accessory.context.device = device;
+                accessory.context.node = node;
+                accessory.context.home = group.name;
+                if (home !== undefined && home.name === group.name) {
+                  this.log.info('Adding new accessory:', accessoryName);
+                  new Radiator(this, accessory, helki);
+                  this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
                 }
-
-                // Accessory exists, restore from cache
-                this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
-                new Radiator(this, existingAccessory, helki);
-              }
-            } else {
-              // Accessory doesn't exist, add new
-              const accessory = new this.api.platformAccessory(accessoryName, accessoryUUID);
-              accessory.context.device = device;
-              accessory.context.node = node;
-              accessory.context.home = group.name;
-              if (home !== undefined && home.name === group.name) {
-                this.log.info('Adding new accessory:', accessoryName);
-                new Radiator(this, accessory, helki);
-                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
-              }
-              if (home === undefined) {
-                this.log.info('Adding new accessory:', accessoryName);
-                new Radiator(this, accessory, helki);
-                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+                if (home === undefined) {
+                  this.log.info('Adding new accessory:', accessoryName);
+                  new Radiator(this, accessory, helki);
+                  this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+                }
               }
             }
           }
         }
-      }
 
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        this.log.error(`Failed to discover devices: ${error.message}`);
-      } else {
-        this.log.error(`Failed to discover devices: ${error}`);
+        // If successful, reset delay
+        backoffDelay = 1000;
+        break;
+
+      } catch (error: unknown) {
+        if (error instanceof Error && error.message.includes('getaddrinfo')) {
+          this.log.error(`Network error encountered: ${error.message}. Retrying in ${backoffDelay / 1000} seconds...`);
+
+          // Wait for the backoff delay before retrying
+          await new Promise(resolve => setTimeout(resolve, backoffDelay));
+
+          // Exponential backoff with a maximum delay of 1 hour
+          backoffDelay = Math.min(backoffDelay * 2, maxBackoffDelay);
+        } else {
+          this.log.error(`Failed to discover devices: ${error instanceof Error ? error.message : error}`);
+          break;
+        }
       }
     }
   }
